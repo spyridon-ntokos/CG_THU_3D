cfeb28a80c57f301d1e46833b93708e24a938086
spyridon-ntokos <spyridon.ntokos@gmail.com>
2021-01-12 17:30:26 +0100

    Intro to main.cpp added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
-/** 
- * Copyright (C) 2018 Tomasz Ga�aj
+﻿/** 
+ Spyridon Ntokos
+ 
+ SARS-CoV-2: COVID-19 3D Model featuring multi-texturing on multiple meshes
+ made in OpenGL/C++ as part of 3D-project for CG subject by Prof. Alfred M. Franz 
+
+ Model created in Blender by Andrew Mezentsew (Teliri) licensed under Creative Commons Attribution 4.0 International (CC BY 4.0)
+ https://sketchfab.com/3d-models/coronavirus-b28f63adc5f04a2aab27e1d7293eba0b
+ 
+ Code-template by Tomasz Gałaj (Copyright 2018)
+ https://shot511.github.io/2018-05-29-how-to-setup-opengl-project-with-cmake/
+ https://github.com/Shot511/OpenGLSampleCmake
+
+ Original code for loading the model with Assimp by LearnOpenGL
+ https://learnopengl.com/Model-Loading/Assimp
+
  **/
 
 #include <iostream>
